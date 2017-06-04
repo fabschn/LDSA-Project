ce2892e91cc90e69eb7d3a979f7a9ec9472cbc3b
Fabian Schöndorff <fabian.schoendorff@googlemail.com>
2017-06-04 16:33:22 +0200

    Cleaning up hadoop scripts.

--- multimapper.py
+++ multimapper.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python3
-
 import sys
 from mrjob.job import MRJob
-
+import logging
+logging.basicConfig(filename='example.log',filemode='w')
 class MRCrimePerYear(MRJob):
-	def filter_data_by_year(self, key, record):
-		if record[18] == 2011:
-			crimeType = record[6]
-			month = record[3]
-			yield crimeType, month
-	def count_event_mapper(self, crime, month)
-		yield'%s %s' % (crimeType, month), 1
+        def filter_data_by_year(self, key, record):
+                record = record.split(',')
+                if record[17] =='2011':
+                        crimeType = record[5]
+                        date = record[2]
+                        month = date.split(' ')[0].split('/')[0]
+                        yield crimeType, month
+        def count_event_mapper(self, crimeType, month):
+                yield'%s %s' % (month, crimeType), 1
 
-	def sumCrime(self, crimeType, month):
-		yield month, sum(crimeType)
+        def sumCrime(self, month, crimeType):
+                yield month, sum(crimeType)
 
-	def steps(self):
-		return [self.mr(mapper=self.filter_data_by_year),
-		mapper=self.count_event_mapper,
-		reducer=self.sumCrime)]
+        def steps(self):
+                return [self.mr(mapper=self.filter_data_by_year),
+                self.mr(mapper=self.count_event_mapper),
+                self.mr(reducer=self.sumCrime)]
 if __name__=='__main__':
-	MRCrimePerYear.run()
-		
+        MRCrimePerYear.run()
 
